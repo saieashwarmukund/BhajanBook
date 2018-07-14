830d6a091d82f776a154ec8eff3ac971fb3a12a7
saieashwarmukund <saieashwar@gmail.com>
2018-07-13 21:08:25 -0700

    search finished, part of contact form done

--- src/org/bhajanbook/dao/BhajanDAO.java
+++ src/org/bhajanbook/dao/BhajanDAO.java
@@ -11,6 +11,11 @@ import org.bhajanbook.service.BhajanLyricsVO;
 import org.bhajanbook.service.BhajanTitleVO;
 
 public class BhajanDAO {
+	
+	public final static int SEARCH_MECH1 = 1;
+	public final static int SEARCH_MECH2 = 2;
+	public final static int SEARCH_MECH3 = 3;
+	
 
 	public List<BhajanTitleVO> getBhajanTitleList(String deity) {
 		Connection conn = null;
@@ -127,7 +132,7 @@ public class BhajanDAO {
 		}
 	}
  
-	public int logBhajanSearch(String searchStr, String userId, String userAgent, String ip, int hits ) {
+	public int logBhajanSearch(String searchStr, String userId, String userAgent, String ip, int searchMech, int hits ) {
 		Connection conn = null;
 		StringBuffer queryStr = null;
 		try {
@@ -136,8 +141,8 @@ public class BhajanDAO {
 			conn.setAutoCommit(false);
 			Statement stmt = conn.createStatement();
 		    queryStr = new StringBuffer("insert into log_bhajan_search ");
-			queryStr.append("(search_str, date_searched, user_agent, remote_addr, user_id, result_hits)");
-			queryStr.append("values ('" + searchStr  + "', now(), '" + userAgent + "', '" + ip + "',null, " + hits + ")");
+			queryStr.append("(search_str, date_searched, user_agent, remote_addr, user_id, search_mech, result_hits)");
+			queryStr.append("values ('" + searchStr  + "', now(), '" + userAgent + "', '" + ip + "',null, " + searchMech + ", " + hits + ")");
 System.out.println(queryStr);
 			int rc = stmt.executeUpdate(queryStr.toString());
 			conn.commit();
@@ -157,15 +162,17 @@ System.out.println(queryStr);
 			}
 		}
 	}
-	public List<BhajanTitleVO> searchBhajan(String searchStr) {
+	public List<BhajanTitleVO> searchBhajan(String searchStr, String userId, String userAgent, String ip, boolean firstTime) {
 		Connection conn = null;
-		ArrayList<BhajanTitleVO> bhajanTitleList = new ArrayList<BhajanTitleVO>();
+		List<BhajanTitleVO> bhajanTitleList = new ArrayList<BhajanTitleVO>();
 		try {
 			searchStr = searchStr.replaceAll("'", "''");
 			// Get DB Connection. 
 			conn = DBConnection.getDBConnection();
 			Statement stmt = conn.createStatement();
 			StringBuffer queryStr = new StringBuffer();
+			
+			// Search based on full search string
 			queryStr.append("select b.bhajan_id, bhajan_title  from bhajandb.bhajan b ");  			
 			queryStr.append("where title_key like '");
 			queryStr.append(searchStr.toUpperCase().trim() + "%'");
@@ -178,12 +185,29 @@ System.out.println(queryStr);
 				btVO.setBhajanTitle(rs.getString("bhajan_title"));
 				bhajanTitleList.add(btVO);
 			}
-			if (bhajanTitleList.size() > 0 || searchStr.length() <= 3) {
+			
+			if (bhajanTitleList.size() > 0 || searchStr.length() <= 5) {
+				if (firstTime) {
+					logBhajanSearch(searchStr, userId, userAgent, ip, SEARCH_MECH1, bhajanTitleList.size());
+				} else {
+					logBhajanSearch(searchStr, userId, userAgent, ip, SEARCH_MECH3, bhajanTitleList.size());
+				}
 				return bhajanTitleList;				
 			}
-			// searchStr = searchStr.substring(0, searchStr.length() - 1);
-			searchStr = searchStr.substring(0, 3);
-			return searchBhajan(searchStr);
+			
+			// search based on sound of first word
+			bhajanTitleList = searchBhajanSoundex(searchStr);
+			if (bhajanTitleList.size() > 0) {
+				logBhajanSearch(searchStr, userId, userAgent, ip, SEARCH_MECH2, bhajanTitleList.size());
+				return bhajanTitleList;
+			}
+			
+			
+			searchStr = searchStr.substring(0, searchStr.length() - 1);
+			// searchStr = searchStr.substring(0, 5);
+			return searchBhajan(searchStr, userId, userAgent, ip, false);
+			// return bhajanTitleList;
+			
 		} catch (Exception e) {
 			// TODO: report error to support.
 			// Return empty list.			
@@ -198,6 +222,55 @@ System.out.println(queryStr);
 			}
 		}
 	}
+	
+	public List<BhajanTitleVO> searchBhajanSoundex(String searchStr) {
+		Connection conn = null;
+		List<BhajanTitleVO> bhajanTitleList = new ArrayList<BhajanTitleVO>();
+		try {
+			searchStr = searchStr.replaceAll("'", "''");
+			
+			// find first word
+			int i = searchStr.indexOf(' ');
+			if (i != -1) {
+				 searchStr = searchStr.substring(0, i);
+			} 
+			
+			// Get DB Connection. 
+			conn = DBConnection.getDBConnection();
+			Statement stmt = conn.createStatement();
+			StringBuffer queryStr = new StringBuffer();
+			queryStr.append("select b.bhajan_id, b.bhajan_title ");
+			queryStr.append("from bhajan b, bhajan_key bk ");
+			queryStr.append("where b.bhajan_id = bk.bhajan_id ");
+			queryStr.append("and soundex_keyword = soundex("+ searchStr.toUpperCase() +") ");
+			queryStr.append("order by bhajan_title ");
+			
+			
+System.out.println(queryStr);
+			ResultSet rs = stmt.executeQuery(queryStr.toString());
+
+			while (rs.next()) {
+				BhajanTitleVO btVO = new BhajanTitleVO();
+				btVO.setId(rs.getInt("bhajan_id"));
+				btVO.setBhajanTitle(rs.getString("bhajan_title"));
+				bhajanTitleList.add(btVO);
+			}
+			return bhajanTitleList;
+		} catch (Exception e) {
+			// TODO: report error to support.
+			// Return empty list.			
+			return bhajanTitleList;
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					// do nothing.
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		BhajanDAO bDAO = new BhajanDAO();
 		try {
